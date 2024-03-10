dfb97022fa99604389487728dcc894e65410d8e6
Domagoj Mutić <domagojmutic@gmail.com>
2024-03-10 21:51:58 +0100

    solution

--- hw02/src/solutions.js
+++ hw02/src/solutions.js
@@ -2,88 +2,145 @@ module.exports = {
   /**
    * Returns an empty object without prototype. There is object creation type that creates object without prototype
    */
-  createPrototypelessObject() {},
+  createPrototypelessObject() {
+    return Object.create(null, {})
+  },
 
   /**
    * Returns an object with prototype set to given `proto`.
    * @param {Object} proto Prototype object
    */
-  createObjectWithPrototype(proto) {},
+  createObjectWithPrototype(proto) {
+    return Object.create(proto, {})
+  },
 
   /**
    * Returns an object with `value` property set to the given `value` and `getValue` method.
    * Be careful, if `value` changes, `getValue` should return changed `value`.
    * @param {any} value
    */
-  createObjectWithMethod(value) {},
+  createObjectWithMethod(value) {
+    return {
+      value,
+      getValue: function() {
+        return this.value;
+      }
+    }
+  },
 
   /**
    * Returns an object with the `getValue` and `setValue` methods, having `value` hidden from the outside.
    */
-  createEncapsulatedObject() {},
+  createEncapsulatedObject() {
+    return function () {
+      let value;
+      return {
+        getValue: function() {
+          return value;
+        },
+        setValue: function(val) {
+          return value = val;
+        }
+      }
+    }()
+  },
 
   /**
    * Returns the shallow copy of the given `obj`. HINT: This **operator** will be used later.
    * @param {Object} obj
    */
-  shallowCopy(obj) {},
+  shallowCopy(obj) {
+    return {...obj};
+  },
 
   /**
    * Returns the deep copy of the given `obj`.
    * @param {Object} obj
    */
-  deepCopy(obj) {},
+  deepCopy(obj) {
+    return structuredClone(obj);
+    // return JSON.parse(JSON.stringify(obj)); //Old way
+  },
 
   /**
    * Returns an array containing 2 elements which are
    * loosely equal, but strictly unequal.
    */
-  looselyTrue() {},
+  looselyTrue() {
+    return [2, "2"];
+  },
 
   /**
    * Returns a string that is loosely equal to boolean `true`. This one is tricky :)
    */
-  stringLooselyEqualToTrue() {},
+  stringLooselyEqualToTrue() {
+    return "1";
+  },
 
   /**
    * Returns correct sum of a and b.
    */
-  safeSum(a, b) {},
+  safeSum(a, b) {
+    return +a + +b;
+  },
 
   /**
    * Returns formatted string for the given date.
    * Format should be `{day}-{month}-{fullYear}` (all numbers).
    * @param {Date} date
    */
-  formatDate(date) {},
+  formatDate(date) {
+    return date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+  },
 
   /**
    * Sorts the given `numberArray` in ascending order.
    * Use array `.sort` method. Sort is done in place so there is no need to return anything.
    * @param {number[]} numberArray
    */
-  sortNumberArray(numberArray) {},
+  sortNumberArray(numberArray) {
+    numberArray.sort((a, b) => a - b);
+  },
 
   /**
    * Multiplies all the elements in the array by 2 _in place_
    * (edits the given array) and returns it.
    * @param {number[]} numberArray
    */
-  multiplyArrayByTwo(numberArray) {},
+  multiplyArrayByTwo(numberArray) {
+    for(let i = 0; i < numberArray.length; i++)
+      numberArray[i] = numberArray[i] * 2;
+    return numberArray;
+  },
 
   /**
    * Multiplies all the elements in the array by 2 and returns them
    * in a new array.
    * @param numberArray
    */
-  multiplyArrayByTwoNew(numberArray) {},
+  multiplyArrayByTwoNew(numberArray) {
+    return numberArray.map(n => n*2);
+  },
 
   /**
    * Returns first `n` Fibonacci numbers in an array. https://en.wikipedia.org/wiki/Fibonacci_sequence
    * If the n is <= 0, return `undefined`
    * @param n
    */
-  fibonacciNumbers(n) {},
+  fibonacciNumbers(n) {
+    if(n <= 0) return undefined;
+
+    let fibonacciArray = [0];
+
+    if(n > 1)
+      fibonacciArray.push(1)
+
+    for(let i = 2; i < n; i++) {
+      fibonacciArray.push(fibonacciArray[i-1] + fibonacciArray[i-2]);
+    }
+    
+    return fibonacciArray;
+  },
 
   /**
    *
@@ -99,7 +156,26 @@ module.exports = {
    * @param {Function} callGetName
    * @param {Function} callGetLanguage
    */
-  classInheritance(callGetName, callGetLanguage) {},
+  classInheritance(callGetName, callGetLanguage) {
+    class Person {
+      constructor(name) {
+        this.name = name
+      }
+      getName() {
+        callGetName(this.name);
+      }
+    }
+    class Programmer extends Person {
+      constructor(name, language) {
+        super(name);
+        this.language = language;
+      }
+      getLanguage() {
+        callGetLanguage(this.language);
+      }
+    }
+    return {Person, Programmer};
+  },
 
   /**
    * **This is variant of probably most common "big firm" interview question with closures.**
@@ -115,9 +191,9 @@ module.exports = {
    */
   timeoutIncrement(consumer) {
     for (var i = 1; i <= 3; i += 1) {
-      setTimeout(() => {
-        /* your function goes here, or instead of this function */
-      }, 1000)
+      setTimeout((n) => {
+        consumer(n);
+      }, i * 1000, i)
     }
   },
 }
