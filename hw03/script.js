04dab49850965fcec124536cf37f7b94609b14b3
Domagoj Mutić <domagojmutic@gmail.com>
2024-03-24 23:38:21 +0100

    added style and upgraded functionality

--- hw03/script.js
+++ hw03/script.js
@@ -1,3 +1,4 @@
+const templatePlayingCard = document.getElementById("template-playing-card");
 const templateQuestion = document.getElementById("template-question");
 const templateAnswer = document.getElementById("template-answer");
 const templateScore = document.getElementById("template-score");
@@ -6,16 +7,55 @@ const quizContent = document.getElementById("quiz-content");
 
 const startButton = document.getElementById("quiz-start");
 
+const loadingDialog = document.getElementById("loading-dialog");
+
+const quizConfig = document.getElementById("quiz-config");
+const noQuestionsRange = document.getElementById("config-questions-range");
+const noQuestions = document.getElementById("config-questions");
+const configCategory = document.getElementById("config-category");
+const configDifficulty = document.getElementById("config-difficulty");
+const configType = document.getElementById("config-type");
+
 let token;
 let error;
-let loading = false;
+let loading = new Proxy(
+    { data: false },
+    {
+        set(obj, prop, value) {
+            if (value === true) loadingDialog.showModal();
+            if (value === false) loadingDialog.close();
+
+            obj.data = value;
+            return true;
+        },
+    }
+);
 let questions = [];
+let cardElements = [];
+let cardScoreElements;
 let answers = [];
+let displayedQuestion;
+let maxQuestions = 15;
 
 startButton.addEventListener("click", async () => {
+    quizContent.innerHTML = "";
+    displayedQuestion = 0;
     await nextQuestion();
-    startButton.style.display = "none";
+    quizConfig.style.display = "none";
+    quizContent.style.display = "";
 });
+
+noQuestionsRange.addEventListener("input", () => {
+    maxQuestions = +noQuestionsRange.value;
+    noQuestions.value = noQuestionsRange.value;
+});
+
+noQuestions.addEventListener("input", () => {
+    maxQuestions = +noQuestions.value;
+    noQuestionsRange.value = noQuestions.value;
+});
+
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 async function evaluateResponse(code, recallFunc) {
     switch (code) {
@@ -29,7 +69,7 @@ async function evaluateResponse(code, recallFunc) {
             await getToken();
             break;
         case 5:
-            await new Promise((r) => setTimeout(r, 5000));
+            await sleep(5000);
             await recallFunc();
             break;
         default:
@@ -38,7 +78,7 @@ async function evaluateResponse(code, recallFunc) {
 }
 
 function getToken() {
-    loading = true;
+    loading.data = true;
     return fetch("https://opentdb.com/api_token.php?command=request")
         .then((res) => {
             if (res.status >= 200) return res.json();
@@ -51,50 +91,68 @@ function getToken() {
             );
             if (responseStatus) {
                 token = jsonResponse.token;
-                loading = false;
+                loading.data = false;
             }
         })
         .catch((err) => {
-            loading = false;
+            loading.data = false;
             error = err;
         });
 }
 
-async function getQuestion(category = "", difficulty = "", type = "") {
-    loading = true;
+async function getQuestion(
+    amount = 1,
+    category = configCategory.value,
+    difficulty = configDifficulty.value,
+    type = configType.value
+) {
     let questionObject;
     if (token === undefined) await getToken();
     await fetch(
-        `https://opentdb.com/api.php?amount=1&token=${token}${
+        `https://opentdb.com/api.php?amount=${amount}&token=${token}${
             category ? "&category=" + category : ""
         }${difficulty ? "&difficulty=" + difficulty : ""}${
             type ? "&type=" + type : ""
         }`
     )
-        .then((res) => {
-            if (res.status >= 200) return res.json();
+        .then(async (res) => {
+            if (res.status >= 200 && res.status <= 299) return res.json();
+            if (res.status == 429) {
+                loading.data = true;
+                await sleep(5000);
+                questionObject = await getQuestion(
+                    amount,
+                    category,
+                    difficulty,
+                    type
+                );
+            }
             throw Error("Fetch error");
         })
         .then(async (jsonResponse) => {
             const responseStatus = await evaluateResponse(
                 jsonResponse.response_code,
-                getQuestion.bind(this, category, difficulty, type)
+                getQuestion.bind(this, amount, category, difficulty, type)
             );
             if (responseStatus) {
                 questionObject = jsonResponse.results;
-                loading = false;
+                loading.data = false;
             }
         })
         .catch((err) => {
-            loading = false;
+            loading.data = false;
             error = err;
         });
 
-    return questionObject[0];
+    return questionObject;
 }
 
-function displayQuestion(question) {
-    quizContent.innerHTML = "";
+function generateQuestionElement(question) {
+    const playingCardContainer = templatePlayingCard.content.cloneNode(true);
+    const playingCard = playingCardContainer.getElementById(
+        "template-playing-card"
+    );
+
     const questionElement = templateQuestion.content.cloneNode(true);
 
     const questionText = questionElement.getElementById(
@@ -104,15 +162,34 @@ function displayQuestion(question) {
         "template-answer-container"
     );
     const nextButton = questionElement.getElementById("template-next");
+    const prevButton = questionElement.getElementById("template-prev");
+    const lockTimer = questionElement.getElementById("template-lock-timer");
 
     questionText.id = `question-text-${question.order}`;
     answerContainer.id = `answer-container-${question.order}`;
     nextButton.id = `next-${question.order}`;
+    playingCard.id = `playing-card-${question.order}`;
 
     questionText.innerHTML = `${question.order + 1}. ${question.question}`;
 
-    nextButton.addEventListener("click", nextQuestion);
+    nextButton.addEventListener("click", () => {
+        displayedQuestion++;
+        nextQuestion();
+    });
 
+    if (question.order === 0) {
+        prevButton.classList.add("opacity-20");
+        prevButton.disabled = true;
+    }
+
+    prevButton.addEventListener("click", () => {
+        cardElements.forEach((e) => (e.style.zIndex = 0));
+        cardElements[displayedQuestion].style.zIndex = 10;
+        displayedQuestion--;
+        hideQuestion(question.order);
+    });
+
+    const answerButtons = [];
     const offeredAnswers = [
         question.correct_answer,
         ...question.incorrect_answers,
@@ -139,19 +216,86 @@ function displayQuestion(question) {
         answerText.innerHTML = answer;
 
         answerButton.addEventListener("click", () => {
-            if (answers[question.order] === answer)
+            if (answers[question.order] === answer) {
                 answers[question.order] = undefined;
-            else answers[question.order] = answer;
+                answerButton.classList.remove(
+                    "bg-green-500",
+                    "text-white",
+                    "hover:bg-green-600"
+                );
+                answerButton.classList.add("hover:bg-gray-50");
+            } else {
+                answers[question.order] = answer;
+                answerButton.classList.add(
+                    "bg-green-500",
+                    "text-white",
+                    "hover:bg-green-600"
+                );
+                answerButton.classList.remove("hover:bg-gray-50");
+                answerButtons.forEach((ab) => {
+                    if (ab !== answerButton) {
+                        ab.classList.remove(
+                            "bg-green-500",
+                            "text-white",
+                            "hover:bg-green-600"
+                        );
+                        ab.classList.add("hover:bg-gray-50");
+                    }
+                });
+            }
         });
 
+        answerButtons.push(answerButton);
         answerContainer.appendChild(answerElement);
     }
 
-    quizContent.appendChild(questionElement);
+    playingCard.appendChild(questionElement);
+    quizContent.appendChild(playingCardContainer);
+    playingCard.style.top = `${+question.order * 2}px`;
+    playingCard.style.setProperty(
+        "--start-rotation",
+        `${Math.random() * 4 - 2}deg`
+    );
+    playingCard.style.setProperty(
+        "--end-rotation",
+        `${Math.random() * 4 - 2}deg`
+    );
+
+    cardElements[question.order] = playingCard;
+
+    // Api can be contacted again after 5 secondes
+    setTimeout(updateTimer, 1000);
+
+    function updateTimer() {
+        let t = +lockTimer.innerHTML;
+        if (t === 1) {
+            nextButton.disabled = false;
+            nextButton.classList.remove("opacity-20");
+            lockTimer.style.display = "none";
+        } else {
+            lockTimer.innerHTML = t - 1;
+            setTimeout(updateTimer, 1000);
+        }
+    }
 }
 
-function displayScore() {
-    quizContent.innerHTML = "";
+function displayQuestion(n) {
+    cardElements[n].classList.add("show");
+    if (cardElements[n].classList.contains("hide"))
+        cardElements[n].classList.remove("hide");
+}
+
+function hideQuestion(n) {
+    cardElements[n].classList.remove("show");
+    cardElements[n].classList.add("hide");
+}
+
+function generateScoreElement() {
+    const playingCardContainer = templatePlayingCard.content.cloneNode(true);
+    const playingCard = playingCardContainer.getElementById(
+        "template-playing-card"
+    );
+
     const scoreElement = templateScore.content.cloneNode(true);
 
     const totalLabel = scoreElement.getElementById("template-total");
@@ -162,15 +306,19 @@ function displayScore() {
     totalLabel.id = `total-label`;
     correctLabel.id = `correct-label`;
     questionList.id = `question-list`;
+    playingCard.id = `playing-card-score`;
 
     homeButton.addEventListener("click", () => {
         error = undefined;
-        loading = false;
+        loading.data = false;
         questions = [];
+        cardElements = [];
         answers = [];
-        startButton.style.display = "";
+        cardScoreElements = undefined;
+        quizConfig.style.display = "";
         quizContent.innerHTML = "";
-    })
+        quizContent.style.display = "none";
+    });
 
     let correct = 0;
     let incorrect = 0;
@@ -180,36 +328,92 @@ function displayScore() {
         else if (answers[i] === questions[i].correct_answer) correct++;
         else incorrect++;
 
-        const questionRow = document.createElement("div");
-        const questionOrderItem = document.createElement("span");
-        const questionAnsweredItem = document.createElement("span");
-        const questionCorrectItem = document.createElement("span");
+        const questionRow = document.createElement("tr");
+        const questionOrderItem = document.createElement("td");
+        const questionAnsweredItem = document.createElement("td");
+        const questionCorrectItem = document.createElement("td");
 
-        questionOrderItem.innerHTML = `${questions[i].order + 1}. / `;
-        questionAnsweredItem.innerHTML = (answers[i] ? answers[i] : "-") + " / ";
+        questionOrderItem.innerHTML = `${questions[i].order + 1}.`;
+        questionAnsweredItem.innerHTML = answers[i] ? answers[i] : "-";
         questionCorrectItem.innerHTML = questions[i].correct_answer;
+
+        if (answers[i] == undefined)
+            questionOrderItem.classList.add("text-yellow-600");
+        else if (answers[i] === questions[i].correct_answer)
+            questionOrderItem.classList.add("text-green-600");
+        else questionOrderItem.classList.add("text-red-600");
 
         questionRow.appendChild(questionOrderItem);
         questionRow.appendChild(questionAnsweredItem);
         questionRow.appendChild(questionCorrectItem);
 
+        questionRow.classList.add(
+            "border-b",
+            "border-gray-200",
+            "hover:bg-gray-50"
+        );
         questionList.appendChild(questionRow);
     }
 
     correctLabel.innerHTML = `${correct}`;
     totalLabel.innerHTML = `${questions.length}`;
 
-    quizContent.appendChild(scoreElement);
+    if (cardScoreElements !== undefined) cardScoreElements.remove();
+
+    cardScoreElements = playingCard;
+
+    playingCard.appendChild(scoreElement);
+    quizContent.appendChild(playingCardContainer);
+
+    playingCard.style.top = `${+questions.length * 2}px`;
+
+    console.log(cardScoreElements);
+}
+
+function displayScore() {
+    generateScoreElement();
+    cardScoreElements.classList.add("show");
 }
 
 async function nextQuestion() {
-    if (questions.length >= 2) {
+    if (displayedQuestion + 1 > maxQuestions) {
+        cardElements.forEach((e) => (e.style.zIndex = 0));
         displayScore();
         return;
     }
-
-    const question = await getQuestion();
-    question.order = questions.length;
-    questions.push(question);
-    displayQuestion(question);
+    if (displayedQuestion + 1 === maxQuestions) generateScoreElement();
+    if (displayedQuestion === 0 && questions[displayedQuestion] === undefined) {
+        const rawQuestions = await getQuestion(2);
+        rawQuestions.forEach((question, i) => {
+            question.order = i;
+            questions[i] = question;
+            generateQuestionElement(question);
+        });
+        displayQuestion(displayedQuestion);
+        cardElements.forEach((e) => (e.style.zIndex = 0));
+        cardElements[displayedQuestion].style.zIndex = 10;
+    } else {
+        if (questions[displayedQuestion] === undefined) {
+            const question = (await getQuestion())[0];
+            question.order = displayedQuestion;
+            questions[displayedQuestion] = question;
+            generateQuestionElement(question);
+            displayQuestion(displayedQuestion);
+            cardElements.forEach((e) => (e.style.zIndex = 0));
+            cardElements[displayedQuestion].style.zIndex = 10;
+        } else {
+            displayQuestion(displayedQuestion);
+            cardElements.forEach((e) => (e.style.zIndex = 0));
+            cardElements[displayedQuestion].style.zIndex = 10;
+        }
+        if (
+            displayedQuestion + 1 < maxQuestions &&
+            questions[displayedQuestion + 1] === undefined
+        ) {
+            const question = (await getQuestion())[0];
+            question.order = displayedQuestion + 1;
+            questions[displayedQuestion + 1] = question;
+            generateQuestionElement(question);
+        }
+    }
 }
