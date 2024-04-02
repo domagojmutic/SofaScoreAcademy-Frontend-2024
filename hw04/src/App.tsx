7c74b8e6fdd3c6854d71181d00cab91452da4bcd
Domagoj Mutić <domagojmutic@gmail.com>
2024-04-03 00:47:13 +0200

    basic colors and fontsize configs & dark theme support

--- hw04/src/App.tsx
+++ hw04/src/App.tsx
@@ -1,11 +1,32 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+type themes = "light" | "dark";
 
 function App() {
+  const [theme, setTheme] = useState<themes>("light");
+
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    )
+      setTheme("dark");
+    else setTheme("light");
+  }, []);
+
+  useEffect(() => {
+    if (theme === "dark") document.documentElement.classList.add("dark");
+    else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
 
   return (
-    <div>
+    <div className="min-w-screen min-h-screen flex flex-col bg-page-lt dark:bg-page-dt">
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- hw04/tailwind.config.js
+++ hw04/tailwind.config.js
@@ -2,8 +2,27 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'header-lt': '#E3350D',
+        'header-dt': '#4D638C',
+        'page-lt': '#EEEEEE',
+        'page-dt': '#354159',
+        'details-lt': '#FFFFFF',
+        'details-dt': '#1F2735',
+        'text-lt': '#262626',
+        'text-dt': '#A1A1A1',
+        'favorite-icon': '#ACACAC',
+      },
+    },
+    fontSize: {
+      base: '1rem',
+      sm: '10px',
+      md: '14px',
+      'lg': '32px',
+    }
   },
   plugins: [],
+  darkMode: 'selector',
 }
 
