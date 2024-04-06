76163ff818dbfd8940475b83427c2515621940d0
Domagoj Mutić <domagojmutic@gmail.com>
2024-04-06 20:03:50 +0200

    changed how themes work & theme selector

--- hw04/src/App.tsx
+++ hw04/src/App.tsx
@@ -1,33 +1,34 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-
-type themes = "light" | "dark";
+import ThemeContext from "./context/ThemeContext";
+import { Themes } from "./models/models";
 
 function App() {
-  const [theme, setTheme] = useState<themes>("light");
+  const [theme, setTheme] = useState<Themes>("auto");
 
   useEffect(() => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    )
-      setTheme("dark");
+    if (!("theme" in localStorage)) {
+      setTheme("auto");
+    } else if (localStorage.theme === "dark") setTheme("dark");
     else setTheme("light");
   }, []);
 
   useEffect(() => {
-    if (theme === "dark") document.documentElement.classList.add("dark");
-    else {
-      document.documentElement.classList.remove("dark");
-    }
+    if (theme === "auto") {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches)
+        document.documentElement.classList.add("dark");
+      else document.documentElement.classList.remove("dark");
+    } else if (theme === "dark") document.documentElement.classList.add("dark");
+    else document.documentElement.classList.remove("dark");
   }, [theme]);
 
   return (
-    <div className="min-w-screen min-h-screen flex flex-col bg-page-lt dark:bg-page-dt">
-      <Header />
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className="min-w-screen min-h-screen flex flex-col bg-page-lt dark:bg-page-dt">
+        <Header />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
