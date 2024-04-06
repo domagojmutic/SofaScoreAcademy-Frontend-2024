665f298076526e8b64b8b128c0d45079b76c052a
Domagoj Mutić <domagojmutic@gmail.com>
2024-04-06 20:06:37 +0200

    adapted for mobile

--- hw04/src/components/Header.tsx
+++ hw04/src/components/Header.tsx
@@ -1,24 +1,38 @@
 import pokeball from "../assets/Pokeball.svg";
 import heart from "../assets/Heart.svg";
 import settings from "../assets/Settings.svg";
+import ConfigMenu from "./ConfigMenu";
 
 function Header() {
   return (
-    <div className="w-full flex flex-row justify-between items-center px-[45px] h-[85px] bg-header-lt dark:bg-header-dt">
+    <div className="sticky top-0 w-full h-[85px] px-[45px] flex flex-row justify-between items-center z-50 phone:h-[45px] phone:pl-[11px] phone:pr-[17px] bg-header-lt dark:bg-header-dt">
       <div className="flex flex-row items-center">
-        <img src={pokeball} alt="Pokeball" />
-        <h1 className="text-page-lt font-semibold text-lg ml-[10px]">
+        <img
+          className="phone:w-[25px] phone:h-[25px]"
+          src={pokeball}
+          alt="Pokeball"
+        />
+        <h1 className="text-page-lt font-semibold text-lg ml-[11px] phone:ml-[9px] phone:text-[18px]">
           POKEDEX
         </h1>
       </div>
       <div className="flex flex-row items-center">
         <button>
-          <img src={heart} alt="Heart" />
+          <img
+            className="phone:w-[25px] phone:h-[25px]"
+            src={heart}
+            alt="Heart"
+          />
         </button>
-        <button className="ml-[28px]">
-          <img src={settings} alt="Settings" />
+        <button className="ml-[28px] phone:ml-[12px]">
+          <img
+            className="phone:w-[25px] phone:h-[25px]"
+            src={settings}
+            alt="Settings"
+          />
         </button>
       </div>
+      <ConfigMenu />
     </div>
   );
 }
